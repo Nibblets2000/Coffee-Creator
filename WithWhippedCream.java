5d488630a9bccefbb76044b48ef1cb9581b82fca
Nibblets2000 <137989849+Nibblets2000@users.noreply.github.com>
2023-06-28 23:41:42 -0700

    Update WithWhippedCream.java

--- WithWhippedCream.java
+++ WithWhippedCream.java
@@ -1,23 +1,41 @@
+/**
+ * Program 'Coffee Creator'
+ * CS160L-1001-1002
+ * @author Noah Thao
+ */
 import java.util.Collections;
 import java.util.List;
 
-public class WithWhippedCream extends CoffeeDecorator {
-    public WithWhippedCream(Coffee c) {
+public class WithSugar extends CoffeeDecorator {
+    private String location;
+    public WithSugar(Coffee c, String location) {
         super(c);
+        this.location = location;
     }
 
     @Override
     public double getCost() {
-        return super.getCost() + 0.25;
+        if (location.contains("Irvine")){
+            return super.getCost() + 1.00;
+        }
+        else if (location.contains("Anaheim")){
+            return super.getCost() + 0.75;
+        }
+        else if (location.contains("San Diego")){
+            return super.getCost() + 0.30;
+        }
+        else {
+            return super.getCost() + 0.15;
+        }
     }
 
     @Override
     public List<String> getIngredients() {
-        return Collections.singletonList("Whipped Cream");
+        return Collections.singletonList("Sugar");
     }
 
     @Override
     public String printCoffee() {
-        return super.printCoffee() + " with whipped cream";
+        return super.printCoffee() + " with sugar";
     }
 }
