a01ab8b35a34a9e669a7206755d62403e02f103b
Nibblets2000 <137989849+Nibblets2000@users.noreply.github.com>
2023-06-28 23:41:04 -0700

    Update WithMilk.java

--- WithMilk.java
+++ WithMilk.java
@@ -1,14 +1,32 @@
+/**
+ * Program 'Coffee Creator'
+ * CS160L-1001-1002
+ * @author Noah Thao
+ */
 import java.util.Collections;
 import java.util.List;
 
 public class WithMilk extends CoffeeDecorator {
-    public WithMilk(Coffee c) {
+    private String location;
+    public WithMilk(Coffee c, String location) {
         super(c);
+        this.location = location;
     }
 
     @Override
     public double getCost() {
-        return super.getCost() + 0.55;
+        if (location.contains("Irvine")){
+            return super.getCost() + 1.90;
+        }
+        else if (location.contains("Anaheim")){
+            return super.getCost() + 1.50;
+        }
+        else if (location.contains("San Diego")){
+            return super.getCost() + 0.99;
+        }
+        else {
+            return super.getCost() + 0.55;
+        }
     }
 
     @Override
