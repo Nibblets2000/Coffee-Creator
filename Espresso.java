f56abe5e7ff4353d662090cae0558b9c423a457a
Nibblets2000 <137989849+Nibblets2000@users.noreply.github.com>
2023-06-28 23:39:05 -0700

    Update Espresso.java

--- Espresso.java
+++ Espresso.java
@@ -1,10 +1,30 @@
+/**
+ * Program 'Coffee Creator'
+ * CS160L-1001-1002
+ * @author Noah Thao
+ */
 import java.util.Collections;
 import java.util.List;
 
 public class Espresso implements Coffee {
+    private String location;
+    Espresso (String location){
+        this.location = location;
+    }
     @Override
     public double getCost() {
-        return 1.75;
+        if (location.contains("Irvine")){
+            return 4.0;
+        }
+        else if (location.contains("Anaheim")){
+            return 3.0;
+        }
+        else if (location.contains("San Diego")){
+            return 2.40;
+        }
+        else {
+            return 1.75;
+        }
     }
 
     @Override
