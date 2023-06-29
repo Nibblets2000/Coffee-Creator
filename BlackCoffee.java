334546a74343a9416686e4a623101fdf528a5e95
Nibblets2000 <137989849+Nibblets2000@users.noreply.github.com>
2023-06-28 23:36:17 -0700

    Update BlackCoffee.java

--- BlackCoffee.java
+++ BlackCoffee.java
@@ -1,10 +1,30 @@
+/**
+ * Program 'Coffee Creator'
+ * CS160L-1001-1002
+ * @author Noah Thao
+ */
 import java.util.Collections;
 import java.util.List;
 
 public class BlackCoffee implements Coffee {
+    private String location;
+    BlackCoffee(String location){
+        this.location = location;
+    }
     @Override
     public double getCost() {
-        return 1.0;
+        if (location.contains("Irvine")){
+            return 2.5;
+        }
+        else if (location.contains("Anaheim")){
+            return 2.25;
+        }
+        else if (location.contains("San Diego")){
+            return 1.50;
+        }
+        else {
+            return 1.0;
+        }
     }
 
     @Override
