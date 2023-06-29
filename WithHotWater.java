858bd8158804dd15a14c81c4087afa931b0d5a41
Nibblets2000 <137989849+Nibblets2000@users.noreply.github.com>
2023-06-28 23:40:43 -0700

    Update WithHotWater.java

--- WithHotWater.java
+++ WithHotWater.java
@@ -1,14 +1,29 @@
+/**
+ * Program 'Coffee Creator'
+ * CS160L-1001-1002
+ * @author Noah Thao
+ */
 import java.util.Collections;
 import java.util.List;
 
 public class WithHotWater extends CoffeeDecorator {
-    public WithHotWater(Coffee c) {
+    private String location;
+    public WithHotWater(Coffee c, String location) {
         super(c);
+        this.location = location;
     }
 
     @Override
     public double getCost() {
-        return super.getCost();
+        if (location.contains("Irvine")){
+            return super.getCost() + 0.50;
+        }
+        else if (location.contains("Anaheim")){
+            return super.getCost() + 0.25;
+        }
+        else {
+            return super.getCost();
+        }
     }
 
     @Override
