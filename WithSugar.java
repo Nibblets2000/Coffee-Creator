043d98314a34531467a1444af2f3caff8b831e70
Nibblets2000 <137989849+Nibblets2000@users.noreply.github.com>
2023-06-28 23:41:25 -0700

    Update WithSugar.java

--- WithSugar.java
+++ WithSugar.java
@@ -1,14 +1,32 @@
+/**
+ * Program 'Coffee Creator'
+ * CS160L-1001-1002
+ * @author Noah Thao
+ */
 import java.util.Collections;
 import java.util.List;
 
 public class WithSugar extends CoffeeDecorator {
-    public WithSugar(Coffee c) {
+    private String location;
+    public WithSugar(Coffee c, String location) {
         super(c);
+        this.location = location;
     }
 
     @Override
     public double getCost() {
-        return super.getCost() + 0.15;
+        if (location.contains("Irvine")){
+            return super.getCost() + 1.00;
+        }
+        else if (location.contains("Anaheim")){
+            return super.getCost() + 0.75;
+        }
+        else if (location.contains("San Diego")){
+            return super.getCost() + 0.30;
+        }
+        else {
+            return super.getCost() + 0.15;
+        }
     }
 
     @Override
